ef8d9ce274ec33f97694f88c1bdb6f4cefd82878
Bryssinck,Robbe R <r.bryssinck@student.fontys.nl>
2020-03-30 16:28:42 +0200

    Added Post model

---
+++ PlanofAction/PlanofAction/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlanofAction.Models
+{
+    public class Post
+    {
+        public int PostID { get; set; }
+        public int ThreadID { get; set; }
+        public int AccountID { get; set; }
+        public string PostMessage { get; set; }
+        public DateTime PostDateCreated { get; set; }
+    }
+}
--- PlanofAction/PlanofAction/ViewModels/ForumPostViewModel.cs
+++ PlanofAction/PlanofAction/ViewModels/ForumPostViewModel.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PlanofAction.Models;
 
 namespace PlanofAction.ViewModels
 {
     public class ForumPostViewModel
     {
+        public string ThreadTitle { get; set; }
+        public string ThreadMessage { get; set; }
+        public string ThreadCategory { get; set; }
+        public string ThreadDateCreated { get; set; }
+        public List<Post> posts { get; set; }
     }
 }
