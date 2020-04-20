4ac4ec9b051869c637cbb508ce1bf62f094adc5d
Bryssinck,Robbe R <r.bryssinck@student.fontys.nl>
2020-04-20 15:14:00 +0200

    Added Action plan context and Dto

---
+++ PlanofAction/DataHandler/ActionPlanDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataHandler
+{
+    public class ActionPlanDto
+    {
+        public int ActionPlanID { get; set; }
+        public int AccountID { get; set; }
+        [Required]
+        [StringLength(300)]
+        public string PlanTitle { get; set; }
+        [Required]
+        [StringLength(10000)]
+        public string PlanMessage { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string PlanCategory { get; set; }
+        public DateTime PlanDateCreated { get; set; }
+    }
+}
