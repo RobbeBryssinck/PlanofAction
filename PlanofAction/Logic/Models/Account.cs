382f90fab3904fe659a56fc061a4aef79e12e222
Bryssinck,Robbe R <r.bryssinck@student.fontys.nl>
2020-05-08 11:05:25 +0200

    models logic

---
+++ PlanofAction/Logic/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PoAModels
+{
+    public class Account
+    {
+        public int AccountID { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public string ProfilePicture { get; set; }
+        public string DateJoined { get; set; }
+    }
+}
---
+++ PlanofAction/Logic/Models/ForumThread.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PoAModels
+{
+    class ForumThread
+    {
+        public int ThreadID { get; set; }
+        public int AccountID { get; set; }
+        public int ForumCategoryID { get; set; }
+        public string ThreadTitle { get; set; }
+        public string ThreadMessage { get; set; }
+        public DateTime ThreadDateCreated { get; set; }
+    }
+}
